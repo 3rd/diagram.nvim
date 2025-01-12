e437306fae05d11d47d9482e1b96181604fa6ee3
Maddison Hellstrom <maddy@na.ai>
2025-01-12 05:33:21 -0800

    feat: add gnuplot renderer

--- lua/diagram/renderers/init.lua
+++ lua/diagram/renderers/init.lua
@@ -1,9 +1,11 @@
 local mermaid = require("diagram/renderers/mermaid")
 local plantuml = require("diagram/renderers/plantuml")
 local d2 = require("diagram/renderers/d2")
+local gnuplot = require("diagram/renderers/gnuplot")
 
 return {
   mermaid = mermaid,
   plantuml = plantuml,
-	d2 = d2,
+  d2 = d2,
+  gnuplot = gnuplot,
 }
